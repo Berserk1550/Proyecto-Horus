ed6f89dd3e6b8339b3a74c43b77197543293d170
LopEmiJuspian <elimy2003@gmail.com>
2025-11-24 19:07:34 -0500

    INICIO OPERACIONES EMILY

--- BD/db_parqueo (5).sql
+++ BD/db_parqueo (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2025 a las 03:19:11
+-- Tiempo de generación: 19-11-2025 a las 01:20:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -76,12 +76,10 @@ CREATE TABLE `registros` (
 CREATE TABLE `tarifas` (
   `id_tarifas` int(11) NOT NULL,
   `parqueadero_nit` varchar(15) DEFAULT NULL,
-  `tipo_tarifa` enum('diurna','nocturna') DEFAULT NULL,
+  `tipo_tarifa` enum('primera_hora','hora_extra','jornada','dia_completo') NOT NULL,
+  `horario` enum('diurno','nocturno') NOT NULL,
   `tipo_vehiculo` enum('carro','moto') DEFAULT NULL,
-  `valor_primera_hora` decimal(7,2) DEFAULT NULL,
-  `valor_hora_extra` decimal(7,2) DEFAULT NULL,
-  `valor_jornada` decimal(10,2) DEFAULT NULL,
-  `valor_dia_completo` decimal(10,2) DEFAULT NULL,
+  `valor_tarifa` int(6) DEFAULT NULL,
   `hora_inicio` time DEFAULT NULL,
   `hora_fin` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -100,7 +98,6 @@ CREATE TABLE `usuarios` (
   `correo` varchar(80) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   `tel_emergencia` varchar(20) DEFAULT NULL,
-  `contrasena` varchar(64) DEFAULT NULL,
   `rol` enum('admin','portero','cliente') DEFAULT NULL,
   `activo` enum('activo','inactivo') DEFAULT 'activo',
   `fecha_registro` datetime DEFAULT current_timestamp()
@@ -110,8 +107,8 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`cedula`, `parqueadero_nit`, `nombres`, `apellidos`, `correo`, `telefono`, `tel_emergencia`, `contrasena`, `rol`, `activo`, `fecha_registro`) VALUES
-('1234567890', '0001', 'Emily', 'Stark', 'e@mail.com', '3120000000', '3111111111', '123', 'admin', 'activo', '2025-10-23 00:00:00');
+INSERT INTO `usuarios` (`cedula`, `parqueadero_nit`, `nombres`, `apellidos`, `correo`, `telefono`, `tel_emergencia`, `rol`, `activo`, `fecha_registro`) VALUES
+('1234567890', '0001', 'Emily', 'Stark', 'e@mail.com', '3120000000', '3111111111', 'admin', 'activo', '2025-10-23 00:00:00');
 
 --
 -- Índices para tablas volcadas
@@ -137,54 +134,17 @@ ALTER TABLE `registros`
 -- Indices de la tabla `tarifas`
 --
 ALTER TABLE `tarifas`
-  ADD PRIMARY KEY (`id_tarifas`),
-  ADD KEY `parqueadero_nit` (`parqueadero_nit`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`cedula`),
-  ADD KEY `parqueadero_nit` (`parqueadero_nit`);
+  ADD PRIMARY KEY (`id_tarifas`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `registros`
---
-ALTER TABLE `registros`
-  MODIFY `id_registros` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `tarifas`
 --
 ALTER TABLE `tarifas`
   MODIFY `id_tarifas` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `registros`
---
-ALTER TABLE `registros`
-  ADD CONSTRAINT `registros_ibfk_2` FOREIGN KEY (`usuario_cedula`) REFERENCES `usuarios` (`cedula`),
-  ADD CONSTRAINT `registros_ibfk_5` FOREIGN KEY (`tarifa_id`) REFERENCES `tarifas` (`id_tarifas`);
-
---
--- Filtros para la tabla `tarifas`
---
-ALTER TABLE `tarifas`
-  ADD CONSTRAINT `tarifas_ibfk_1` FOREIGN KEY (`parqueadero_nit`) REFERENCES `parqueadero` (`nit`);
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`parqueadero_nit`) REFERENCES `parqueadero` (`nit`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ models/m_operaciones.py
@@ -0,0 +1,29 @@
+from conexion import *
+from datetime import datetime
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+class Operaciones:
+    def ingreso(self, vehiculo_placa, usuario_cedula, parqueadero_nit):
+        fecha_ingreso = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        sql = """INSERT INTO registros (parqueadero_nit, usuario_cedula, vehiculo_placa, fecha_ingreso) VALUES (%s, %s, %s, %s)"""
+        mi_cursor.execute(sql,(parqueadero_nit, usuario_cedula, vehiculo_placa, fecha_ingreso))
+        mi_db.commit()
+
+    def salida(self, vehiculo_placa):
+        fecha_salida = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        sql = """UPDATE registros SET fecha_salida =%s WHERE vehiculo_placa = %s AND fecha_salida IS NULL"""
+        mi_cursor.execute(sql,(fecha_salida, vehiculo_placa))
+        mi_db.commit()
+
+    def vehiculos_activos(self, parqueadero_nit):
+        sql = """SELECT id_registros, vehiculo_placa, usuario_cedula, fecha_ingreso FROM registros WHERE parqueadero_nit = %s AND fecha_salida IS NULL ORDER BY fecha_ingreso DESC"""
+        mi_cursor.execute(sql,(parqueadero_nit))
+        return mi_cursor.fetchall()
+
+    def registros_previos(self, parqueadero_nit):
+        sql = """SELECT id_registros, vehiculo_placa, parqueadero_nit, fecha_ingreso, fecha_salida FROM registros WHERE parqueadero_nit= %s AND fecha_salida IS NOT NULL ORDER BY fecha_salida DESC"""
+        mi_cursor.execute(sql,(parqueadero_nit))
+        return mi_cursor.fetchall()
+
+mi_operacion = Operaciones()
